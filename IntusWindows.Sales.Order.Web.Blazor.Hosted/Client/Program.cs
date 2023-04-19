6468882b29fdca38e311b17ee512a05c0deb649e
Abdullah Al Mamun <mamunaiub@hotmail.com>
2023-04-19 14:17:46 +0600

    Added hosted project with signlr

---
+++ IntusWindows.Sales.Order.Web.Blazor.Hosted/Client/Program.cs
@@ -0,0 +1,11 @@
+using IntusWindows.Sales.Order.Web.Blazor.Hosted.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
---
+++ IntusWindows.Sales.Order.Web.Blazor.Hosted/Shared/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace IntusWindows.Sales.Order.Web.Blazor.Hosted.Shared
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string? Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
